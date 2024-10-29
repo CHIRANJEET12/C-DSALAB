f1ddf958e5dbc2fb2efc028483081cfa99fb9dd4
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-10-29 17:29:30 +0530

    commit push

--- ninth/q20.c
+++ ninth/q20.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
+
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
@@ -12,20 +13,23 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    for (int i=0;i<n-1;i++) {
-        for (int j=0;j<n-i-1;j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j+1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
+    for(int i=0;i<n-1;i++){
+        int min = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min = j;
             }
         }
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
     }
 
     printf("Sorted array :\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+
 
     return 0;
 }
--- ninth/q21.c
+++ ninth/q21.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
+
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
@@ -12,14 +13,15 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    for (int i=0;i<n-1;i++) {
-        for (int j=0;j<n-i-1;j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j+1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
-            }
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j=i-1;
+
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j=j-1;
         }
+        arr[j+1]=key;
     }
 
     printf("Sorted array :\n");
